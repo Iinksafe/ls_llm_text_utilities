491c0b3a6f053a40df1ae432d5f9880338a54c31
Link Safe <linksafebot@gmail.com>
2024-03-25 23:05:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from unicodedata import category
+from secrets import randbelow
+from .classes import *
+
+
+def bits(int: int): return (7 + int.bit_length()) // 8
+
+
+def scramble(data: Any, /, *, output: tuple[Any, ...] = ()) -> tuple[Any, ...]:
+    while data:
+        char = randbelow(len(data))
+        output += data[char],
+        data = data[:char] + data[1 + char:]
+    return output
+
+
+def tokenize(text: str, /, *, j: MethodType = ''.join) -> map:
+    tokens = [text[0]],
+    for token in text[1:]:
+        if category(token) != category(tokens[-1][-1]): tokens += [token],
+        else: tokens[-1].append(token)
+    return map(j, tokens)
